641654e77de6163db21231aca3f9c78e04789ad8
jimmy <jimmy@localhost>
2020-09-25 12:10:02 +0800

    Added lots push

---
+++ submittogithub/src/submittogithub/submitfile.java
@@ -0,0 +1,16 @@
+package submittogithub;
+
+public class submitfile {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hi, my name is Jimmy Liu") ;
+		System.out.println("I am fourteen years old.") ;
+		System.out.println("I like basketball, golf, coding, playing guitar and playing clarinet.");
+		System.out.println("I am from New Jersey, Livingston but due to the covid crisis I moved back to China for now.");
+		System.out.println("I am very excited to learn some more Java!");
+		
+		
+	}
+	
+}
